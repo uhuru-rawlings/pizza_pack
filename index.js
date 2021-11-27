359b251d9cd60133be224464de5e52d607b36c89
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-27 09:07:41 +0300

    added comment section

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ const validate = () =>{
     var names = document.getElementById("username").value.trim();
     var usermail = document.getElementById("usermail").value.trim();
     var messages = document.getElementById("messages").value.trim();
+    if(names === "" || usermail === "" || messages === ""){
+        alert("please fill all fields");
+        return false;
+    }else{
+        alert("Thanks for contacting us, a reply will be sent to your email");
+        document.getElementById("username").value="";
+        document.getElementById("usermail").value="";
+        document.getElementById("messages").value="";
+        return false;
+    }
 }
