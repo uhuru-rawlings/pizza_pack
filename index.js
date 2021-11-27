07e84289c522c2a993a425d0dc2ee477edd4b643
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-27 09:02:41 +0300

    added form validation with javascript

---
+++ index.js
@@ -0,0 +1,5 @@
+const validate = () =>{
+    var names = document.getElementById("username").value.trim();
+    var usermail = document.getElementById("usermail").value.trim();
+    var messages = document.getElementById("messages").value.trim();
+}
