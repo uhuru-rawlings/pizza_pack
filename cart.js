861105bdf9d436213ced22c8eae873f5db7e349b
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-28 08:28:42 +0300

    added cart content

--- cart.js
+++ cart.js
@@ -33,7 +33,7 @@ function getbuyid(clicked_id){
     if(gotten == "SicilianPizza"){
         var confirms = confirm("Name: "+ itemandPrice.SicilianPizza[0] + " "+ "Price: "+itemandPrice.SicilianPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-             var Item1 = ["Sicilian Pizza",950];
+             var Item1 = "SicilianPizza";
              localStorage.setItem("Item1",Item1);
              alert("added to cart");
            }else{
@@ -42,7 +42,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "StLouisPizza"){
         var confirms = confirm("Name: "+ itemandPrice.StLouisPizza[0] + " "+ "Price: "+itemandPrice.StLouisPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item2 = ["St. Louis Pizza",1000];
+               var Item2 = "StLouisPizza";
                localStorage.setItem("Item2",Item2);
                alert("added to cart");
            }else{
@@ -51,7 +51,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "CaliforniaPizza"){
         var confirms = confirm("Name: "+ itemandPrice.CaliforniaPizza[0] + " "+ "Price: "+itemandPrice.CaliforniaPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item3 = ["California Pizza",1000];
+               var Item3 = "CaliforniaPizza";
                localStorage.setItem("Item3",Item3);
                alert("added to cart");
            }else{
@@ -60,7 +60,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "ChicagoPizza"){
         var confirms = confirm("Name: "+ itemandPrice.ChicagoPizza[0] + " "+ "Price: "+itemandPrice.ChicagoPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item4 = ["Chicago Pizza",900];
+               var Item4 = "ChicagoPizza";
                localStorage.setItem("Item4",Item4);
                alert("added to cart");
            }else{
@@ -69,7 +69,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "DetroitPizza"){
         var confirms = confirm("Name: "+ itemandPrice.DetroitPizza[0] + " "+ "Price: "+itemandPrice.DetroitPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item5 = ["Detroit Pizza",850];
+               var Item5 = "DetroitPizza";
                localStorage.setItem("Item5",Item5);
                alert("added to cart");
            }else{
@@ -78,7 +78,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "GreekPizza"){
         var confirms = confirm("Name: "+ itemandPrice.GreekPizza[0] + " "+ "Price: "+itemandPrice.GreekPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item6 = ["Greek Pizza",950];
+               var Item6 = "GreekPizza";
                localStorage.setItem("Item6",Item6);
                alert("added to cart");
            }else{
@@ -87,7 +87,7 @@ function getbuyid(clicked_id){
     }else if(gotten == "NeapolitanPizza"){
         var confirms = confirm("Name: "+ itemandPrice.NeapolitanPizza[0] + " "+ "Price: "+itemandPrice.NeapolitanPizza[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item7 = ["Neapolitan Pizza",1000];
+               var Item7 = "NeapolitanPizza";
                localStorage.setItem("Item7",Item7);
                alert("added to cart");
            }else{
@@ -96,7 +96,7 @@ function getbuyid(clicked_id){
     }else{
         var confirms = confirm("Name: "+ itemandPrice.newyork[0] + " "+ "Price: "+itemandPrice.newyork[1]+ " "+"Quantity" + " " +1);
            if(confirms == true){
-               var Item8 = ["New York-Style Pizza",1000];
+               var Item8 = "newyork";
                localStorage.setItem("Item8",Item8);
                alert("added to cart");
            }else{
