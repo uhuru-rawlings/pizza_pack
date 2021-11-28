32355ee2bcf3c8db795e625a47b9f2009c878e93
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-28 11:52:37 +0300

    added cart js

--- storage.js
+++ storage.js
@@ -1,16 +1,207 @@
-var Objects = {
-    SicilianPizza: ["Sicilian Pizza","false",950],
-    StLouisPizza: ["St. Louis Pizza","false",1000],
-    CaliforniaPizza: ["California Pizza","false",1000],
-    ChicagoPizza: ["Chicago Pizza","false",900],
-    DetroitPizza: ["Detroit Pizza","false",850],
-    GreekPizza: ["Greek Pizza","false",950],
-    NeapolitanPizza: ["Neapolitan Pizza","false",1000],
-    newyork: ["New York-Style Pizza","false",1000]
-};
-var storeditems = localStorage.getItem("StoredItems");
-if(storeditems){
+const increments = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity").innerText);
+    var currentPrice = parseInt(document.getElementById("price1").innerText);
+    var totalPrice = document.getElementById("tottla1").innerText = currentPrice;
 
-} else{
-    localStorage.setItem("StoredItems",Objects);
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla1").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity").innerText);
+    var currentPrice = parseInt(document.getElementById("price1").innerText);
+    var totalPrice = document.getElementById("tottla1").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla1").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity").innerText = curentQuantity - 1;
+            document.getElementById("tottla1").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// second item
+const increments1 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity2").innerText);
+    var currentPrice = parseInt(document.getElementById("price2").innerText);
+    var totalPrice = document.getElementById("tottla2").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity2").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla2").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements1 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity2").innerText);
+    var currentPrice = parseInt(document.getElementById("price2").innerText);
+    var totalPrice = document.getElementById("tottla2").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla2").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity2").innerText = curentQuantity - 1;
+            document.getElementById("tottla2").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// thirditem
+const increments2 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity3").innerText);
+    var currentPrice = parseInt(document.getElementById("price3").innerText);
+    var totalPrice = document.getElementById("tottla3").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity3").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla3").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements2 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity3").innerText);
+    var currentPrice = parseInt(document.getElementById("price3").innerText);
+    var totalPrice = document.getElementById("tottla3").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla3").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity3").innerText = curentQuantity - 1;
+            document.getElementById("tottla3").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// fouth
+const increments3 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity4").innerText);
+    var currentPrice = parseInt(document.getElementById("price4").innerText);
+    var totalPrice = document.getElementById("tottla4").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity4").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla4").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements3 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity4").innerText);
+    var currentPrice = parseInt(document.getElementById("price4").innerText);
+    var totalPrice = document.getElementById("tottla4").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla4").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity4").innerText = curentQuantity - 1;
+            document.getElementById("tottla4").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// fifth
+const increments4 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity5").innerText);
+    var currentPrice = parseInt(document.getElementById("price5").innerText);
+    var totalPrice = document.getElementById("tottla5").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity5").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla5").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements4 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity5").innerText);
+    var currentPrice = parseInt(document.getElementById("price5").innerText);
+    var totalPrice = document.getElementById("tottla5").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla5").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity5").innerText = curentQuantity - 1;
+            document.getElementById("tottla5").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// sixth
+const increments5 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity6").innerText);
+    var currentPrice = parseInt(document.getElementById("price6").innerText);
+    var totalPrice = document.getElementById("tottla6").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity6").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla6").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements5 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity6").innerText);
+    var currentPrice = parseInt(document.getElementById("price6").innerText);
+    var totalPrice = document.getElementById("tottla6").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla6").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity6").innerText = curentQuantity - 1;
+            document.getElementById("tottla6").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// seventh
+const increments6 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity7").innerText);
+    var currentPrice = parseInt(document.getElementById("price7").innerText);
+    var totalPrice = document.getElementById("tottla7").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity7").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla7").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements6 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity7").innerText);
+    var currentPrice = parseInt(document.getElementById("price7").innerText);
+    var totalPrice = document.getElementById("tottla7").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla7").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity7").innerText = curentQuantity - 1;
+            document.getElementById("tottla7").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
+}
+// eighth
+const increments7 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity8").innerText);
+    var currentPrice = parseInt(document.getElementById("price8").innerText);
+    var totalPrice = document.getElementById("tottla8").innerText = currentPrice;
+
+    if(curentQuantity == 1 || curentQuantity <= 4){
+        document.getElementById("itemquantity8").innerText = curentQuantity + 1;
+    }
+    document.getElementById("tottla8").innerText = (curentQuantity + 1) * totalPrice;
+}
+const decrements7 = () =>{
+    var curentQuantity = parseInt(document.getElementById("itemquantity8").innerText);
+    var currentPrice = parseInt(document.getElementById("price8").innerText);
+    var totalPrice = document.getElementById("tottla8").innerText = currentPrice;
+
+    if(curentQuantity >= 1){
+        if(curentQuantity == 0){
+            curentQuantity = 0;
+            document.getElementById("tottla8").innerText = 0;
+        }else{
+            var newcurentQuantity = document.getElementById("itemquantity8").innerText = curentQuantity - 1;
+            document.getElementById("tottla8").innerText = (newcurentQuantity - 1) * totalPrice;
+        }
+    }
 }
